79805f713896a6596768d2c0923b12ae00cdf8a1
MhdSyrwan <mhdsyrwan@gmail.com>
2012-12-10 23:38:35 +0200

    Created String Class

---
+++ src/lang/String.java
@@ -0,0 +1,29 @@
+package lang;
+
+import parser.Operator.OperatorType;
+import lang.JavaScriptObject;
+
+public class String extends JavaScriptObject {
+
+	@Override
+	public lang.JavaScriptObject operator(OperatorType type,
+			lang.JavaScriptObject right) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public lang.JavaScriptObject operator(OperatorType type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

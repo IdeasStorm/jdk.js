6f58333abccaef8d6f9c3032230e0f6ed6660272
MhdSyrwan <mhdsyrwan@gmail.com>
2012-12-10 23:38:17 +0200

    added operators operands autoboxing

--- src/lang/JavaScriptObject.java
+++ src/lang/JavaScriptObject.java
@@ -20,6 +20,13 @@ public abstract class JavaScriptObject {
 			return left.getValue().operator(type, right.getValue());
 	}
 	
+	@SuppressWarnings("rawtypes")
+	public static Class getResultClass(Class class1, Class class2) {
+		if (class1 == String.class || class2 == String.class)
+			return String.class;
+		return class1;
+	}
+	
 	public abstract JavaScriptObject operator(parser.Operator.OperatorType type, JavaScriptObject right);
 	
 	public abstract JavaScriptObject operator(parser.Operator.OperatorType type);

07e038769f3a01fa04e87f23e0180a0eea8f8d8a
MhdSyrwan <mhdsyrwan@gmail.com>
2012-12-28 14:23:58 +0200

    added ArrayType,FunctionType Objects and Function Node

---
+++ src/parser/FunctionDecarationStatement.java
@@ -0,0 +1,19 @@
+package parser;
+
+public class FunctionDecarationStatement extends JavaScriptNode {
+
+	@Override
+	public void execute(Context context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

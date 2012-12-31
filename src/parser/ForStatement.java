6ad15804e955111e73708070be12332334dd6e70
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-01-01 01:01:57 +0200

    Renamed variable and removed TODO in For Statement class

--- src/parser/ForStatement.java
+++ src/parser/ForStatement.java
@@ -1,28 +1,27 @@
 package parser;
 
 public class ForStatement extends Statement {
-	ExpressionNode frExprssion;
-	ExpressionNode sdExpression; 
-	ExpressionNode thdExpression;
+	ExpressionNode firstExprssion;
+	ExpressionNode secondExpression; 
+	ExpressionNode thirdExpression;
 	Statement statement;
 	
 	public ForStatement(ExpressionNode frExprssion, ExpressionNode sdExpression, 
 			ExpressionNode thdExpression, Statement statement) {
-		this.frExprssion = frExprssion;
-		this.sdExpression = sdExpression;
-		this.thdExpression = thdExpression;
+		this.firstExprssion = frExprssion;
+		this.secondExpression = sdExpression;
+		this.thirdExpression = thdExpression;
 		this.statement = statement;
 	}
 	
 	@Override
 	public Trilogy execute(Context context) {
 		BlockContext forStatementContext = new BlockContext(context); 
-		frExprssion.execute(forStatementContext);
-		while (sdExpression.evaluate(context).toBooleanType().toBoolean()) {
+		firstExprssion.execute(forStatementContext);
+		while (secondExpression.evaluate(context).toBooleanType().toBoolean()) {
 			Trilogy trilogy = statement.execute(forStatementContext);
-			thdExpression.execute(forStatementContext);
+			thirdExpression.execute(forStatementContext);
 			if (trilogy.type == Trilogy.Type.Break)
-				//TODO V=stmt.value 
 				return new Trilogy(Trilogy.Type.Normal, null, null);
 		}
 		return new Trilogy(Trilogy.Type.Normal, null, null);

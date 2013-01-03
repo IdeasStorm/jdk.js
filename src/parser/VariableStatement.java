3dc0d7653018ade08bd4d3f5b69f4436c3c86399
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-01-04 00:52:15 +0200

    Used list of pair in variale statement

--- src/parser/VariableStatement.java
+++ src/parser/VariableStatement.java
@@ -3,16 +3,16 @@ package parser;
 import java.util.*;
 
 public class VariableStatement extends Statement {
-	private List<Pair<String, ExpressionNode>> variables;
+	private List<Pair> variables;
 	
-	public VariableStatement(List<Pair<String, ExpressionNode>> variables) {
+	public VariableStatement(List<Pair> variables) {
 		this.variables = variables;
 	}
 	
 	@Override
 	public Trilogy execute(Context context) {
 		// for each variable in Hash
-		for(Pair<String, ExpressionNode> pair : variables) {
+		for(Pair pair : variables) {
 			// Add variable to context 
 			context.defineVariable(pair.getLeft(), pair.getRight().evaluate(context));
 		}

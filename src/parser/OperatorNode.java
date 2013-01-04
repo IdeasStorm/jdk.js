95c0cb327b7ab5f7af0588123d5270b26f89c5a5
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-01-04 14:22:08 +0200

    Added post increment, pre increment, post decrement and pre decrement

--- src/parser/OperatorNode.java
+++ src/parser/OperatorNode.java
@@ -1,11 +1,25 @@
 package parser;
 
+import java.util.HashSet;
+import java.util.Set;
+import lang.BooleanType;
 import lang.ObjectType;
 
 public class OperatorNode extends ExpressionNode {
 	protected ExpressionNode left;
 	protected ExpressionNode right;
 	protected OperatorType type;
+	
+	protected Set<OperatorType> leftHandSet = new HashSet<OperatorType>() {{
+	    add(OperatorType.PostDecrement);
+	    add(OperatorType.PostIncrement);
+	}};
+	
+	protected Set<OperatorType> rightHandSet = new HashSet<OperatorType>() {{
+	    add(OperatorType.PreDecrement);
+	    add(OperatorType.PreIncrement);
+	}};
+	
 	public enum OperatorType {
 		Add, //+
 		PostIncrement, // i++
@@ -58,16 +72,34 @@ public class OperatorNode extends ExpressionNode {
 	}
 	
 	
+	private void postPreIncDec(ExpressionNode left, ExpressionNode right, Context context) {
+		if ((type == OperatorType.PostIncrement) ||(type == OperatorType.PreIncrement))
+			value = ObjectType.operator(OperatorType.Add, left.evaluate(context), 
+					LiteralNode.createNumberLiteral("1").evaluate(context));
+		else
+			value = ObjectType.operator(OperatorType.Subtract, left.evaluate(context), 
+					LiteralNode.createNumberLiteral("1").evaluate(context));
+		ObjectType.operator(OperatorType.Assignment, left.evaluate(context), value);
+	}
+	
 	public Trilogy execute(Context context) {
 		// WARNING DONT dereference ON LEFT SIDE
 		if (type == OperatorType.Assignment) {
 			ObjectType leftVal = left.evaluate(context);
 			if (isref(leftVal))
 				ObjectType.operator(type,leftVal,deref(right.evaluate(context)));
+			else if ((left == null) && (right != null))
+				BooleanType.operator(type, null, right.evaluate(context));
 			else
 				throw new RuntimeException("ReferenceError: Invalid left-hand side in assignment");
 		}
-		else 
+		else if (leftHandSet.contains(type)) { // i++ and i--
+			postPreIncDec(left, right, context);
+		}
+		else if (rightHandSet.contains(type)) {
+			postPreIncDec(right, left, context);
+		}
+		else
 			value = ObjectType.operator(type,deref(left.evaluate(context)),deref(right.evaluate(context)));
 		return new Trilogy(null, null, null);
 	}

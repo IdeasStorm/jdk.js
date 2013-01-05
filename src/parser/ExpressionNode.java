f3df38a2a13ae655a022ccc10500da29debd35fb
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-01-05 19:11:50 +0200

    Fixed return, break, ...etc. bugs

--- src/parser/ExpressionNode.java
+++ src/parser/ExpressionNode.java
@@ -9,7 +9,9 @@ public abstract class ExpressionNode extends JavaScriptNode {
 	}
 	
 	public ObjectType evaluate(Context context) {
-		this.execute(context);
+		StatementStatus status = this.execute(context);
+		if (status.getType() == StatementStatus.Type.Normal)
+			throw new RuntimeException("SytaxError "+ status.getType() + " statement");
 		return value;
 	}
 }
